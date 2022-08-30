69b38587dc6144d7cc1f5761df68facdbd0a67b4
DEV_FINWIZ <78873223+devfinwiz@users.noreply.github.com>
2022-08-30 12:38:20 +0530

    Time: 212 ms (99.63%), Space: 15 MB (93.31%) - LeetHub

---
+++ 639-decode-ways-ii/639-decode-ways-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        answer=1
+        prev=1
+        one,two=False,False
+        
+        for i in s:
+            if(i=='*'):
+                new=answer*9
+                if(one):
+                    new+=9*prev
+                if(two):
+                    new+=6*prev
+                one,two=True,True
+            else:
+                new=answer if(i>'0') else 0
+                if(one):
+                    new+=prev
+                if(two and i<='6'):
+                    new+=prev
+                one=(i=='1')
+                two=(i=='2')
+                
+            prev=answer
+            answer=new%(10**9+7)
+            
+        return answer
