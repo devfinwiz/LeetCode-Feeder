f12b62218669f1e11704e7bc1856bba05127cf4a
DEV_FINWIZ <78873223+devfinwiz@users.noreply.github.com>
2022-09-19 18:14:15 +0530

    Time: 1121 ms (7.93%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select a.id as id from weather a,weather b where a.temperature>b.temperature and datediff(a.recordDate,b.recordDate)=1;
+select a.id as Id from weather a,weather b where a.temperature>b.temperature and datediff(a.recordDate,b.recordDate)=1;
