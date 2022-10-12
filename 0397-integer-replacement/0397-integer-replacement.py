c3048b23ab58cf64ce544d927c604904d997e536
DEV_FINWIZ <78873223+devfinwiz@users.noreply.github.com>
2022-10-12 15:55:29 +0530

    Time: 55 ms (60.83%), Space: 13.8 MB (67.90%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        count=0
+        while(n>1):
+            if(n==3):
+                return count+2
+            if(n%2==0):
+                n//=2
+            elif(n%4==1):
+                n-=1
+            else:
+                n+=1
+            count+=1
+        
+        return count
+        
+        
+        
