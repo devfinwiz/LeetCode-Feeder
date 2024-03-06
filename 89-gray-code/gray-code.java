602988f79c5a91857d3a0bd700693d5ecd018aaa
Jay Shinde <82300933+jayshinde10@users.noreply.github.com>
2024-03-06 19:48:53 +0530

    Time: 7 ms (68.35%) | Memory: 51.6 MB (43.17%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+
+        for (int i = 0; i < n; i++) {
+            int size = result.size();
+            for (int j = size - 1; j >= 0; j--) {
+                result.add(result.get(j) | (1 << i));
+            }
+        }
+
+        return result;
+    }
+}
