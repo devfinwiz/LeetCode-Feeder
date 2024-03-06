c1ed1fe9e64700461ae3a8180fabdceb06548ae0
sumedh2424 <83491168+sumedh2424@users.noreply.github.com>
2024-03-06 21:46:27 +0530

    Time: 78 ms (63.99%) | Memory: 70.2 MB (52.88%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.size(); ++i) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
