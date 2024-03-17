e0eec42e0278432548638ec17e7bd5496eb1300c
sumedh2424 <83491168+sumedh2424@users.noreply.github.com>
2024-03-17 10:52:29 +0530

    Time: 21 ms (81.39%) | Memory: 15.7 MB (92.84%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target) {
+        std::vector<std::vector<int>> result;
+        int n = nums.size();
+        
+        if (n < 4)
+            return result;
+        
+        std::sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) // Skip duplicates
+                continue;
+            
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) // Skip duplicates
+                    continue;
+                
+                int left = j + 1;
+                int right = n - 1;
+                
+                while (left < right) {
+                    long long sum = static_cast<long long>(nums[i]) + nums[j] + nums[left] + nums[right];
+                    
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        
+                        // Skip duplicates
+                        while (left < right && nums[left] == nums[left + 1])
+                            ++left;
+                        while (left < right && nums[right] == nums[right - 1])
+                            --right;
+                        
+                        ++left;
+                        --right;
+                    } else if (sum < target) {
+                        ++left;
+                    } else {
+                        --right;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+};
