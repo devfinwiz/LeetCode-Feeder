3c82fab84db21884bc86febb23e084bc78b88bac
sumedh2424 <83491168+sumedh2424@users.noreply.github.com>
2024-05-31 23:18:02 +0530

    Time: 23 ms (44.13%) | Memory: 27.8 MB (45.22%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+private:
+    stack<TreeNode*> stk;
+
+    void pushAllLeft(TreeNode* node) {
+        while (node != nullptr) {
+            stk.push(node);
+            node = node->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushAllLeft(root);
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* current = stk.top();
+        stk.pop();
+        pushAllLeft(current->right);
+        return current->val;
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !stk.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
